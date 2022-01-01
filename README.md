862a4ec34407118a9ad36cc51c2feeb75a0d9b6b
smshack <5432tat@naver.com>
2022-01-01 23:26:35 +0900

    비디오 썸네일 타이틀 채널 목록 + css

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 https://northern-breath-ec3.notion.site/2-4266d4b5bcc44a468363f4d2f3592aad
 
 2. 비디오 목록 보여주기
+
+3. 비디오 썸네일 타이틀 채널 목록 + css
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './video_item.module.css'
+const VideoItem = ({ video: { snippet } }) => {
+    // const {thumbnails, title, channelTitle} = video.snippet
+    return (
+        <li className={styles.container}>
+            <div className={styles.video}>
+                <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video item" />
+                <div className={styles.metadata}>
+                    <p className={styles.title}>{snippet.title}</p>
+                    <p className={styles.channel}>{snippet.channelTitle}</p>
+                </div>
+            </div>
+        </li>
+    )
 
-const VideoItem = (props) => (
-            <h1>{props.video.snippet.title}</h1>
-    );
+};
 
 export default VideoItem;
