43d5c1a7527ba33cdb419aed2924b95a8bd7943a
smshack <5432tat@naver.com>
2022-01-02 00:44:56 +0900

    유투브 검색창 만들기 + api key env로 빼기 + service 분리하기

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://northern-breath-ec3.notion.site/2-4266d4b5bcc44a468363f4d2f3592aad
 2. 비디오 목록 보여주기
 
 3. 비디오 썸네일 타이틀 채널 목록 + css
+https://cssmatic.com/
+
+4. 검색 기능 구현하기
--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,27 @@
-import React, { Component,useState ,useEffect } from 'react';
-import './app.css';
+import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
-const App = (props) => {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  useEffect(()=>{
-    const requestOptions ={
-      method:'GET',
-      redirect:'follow'
-    };
-    fetch(
-      'https://www.googleapis.com/youtube/v3/search?q=bts&part=snippet&maxResults=25&key=AIzaSyCkgJsSX6Ii4TUZJzSDM6l7rrrOCB1cH5U',
-      requestOptions
-    )
-    .then(response => response.json())
-    .then(result => setVideos(result.items))
-    .catch(error => console.log(error,'error'))
-  },[])
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
+  };
+
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
+  }, []);
   return (
-    <>
-    <VideoList videos={videos}/>
-    </>
-  )
-};
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
+}
 
 export default App;
